810a22955cf1ed905cd80a854e4fbca7f4ac4931
Shanawaz Islam <acarif@gmail.com>
2021-04-29 05:43:06 +0200

    Added babel and basic layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,50 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/app/app.tsx',
-  devtool: 'inline-source-map',
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
   plugins: [
     new CleanWebpackPlugin({
-      cleanAfterEveryBuildPatterns: ['public/build']
+      cleanAfterEveryBuildPatterns: ["public/build"],
     }),
     new HtmlWebpackPlugin({
-      template: 'src/templates/index.html'
+      template: "src/templates/index.html",
     }),
   ],
   output: {
-    path: __dirname + '/dist',
-    filename: 'build/[name].[contenthash].js'
+    path: __dirname + "/dist",
+    filename: "build/[name].[contenthash].js",
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: [".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: 'ts-loader' }
-    ]
-  }
-}
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      {
+        test: /\.css?$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.md$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+          {
+            loader: "markdown-loader",
+            options: {
+              /* your options here */
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
